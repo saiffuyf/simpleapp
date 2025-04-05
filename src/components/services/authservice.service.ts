59483989173db8a4fa25d766ae9abc1f1db2a4b5
saikiran601 <bommidenisaikiran423@gmail.com>
2025-04-05 19:10:16 +0530

    modified full urls

--- src/components/services/authservice.service.ts
+++ src/components/services/authservice.service.ts
@@ -7,14 +7,18 @@ import { HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class AuthserviceService {
-  private apiUrl = 'http://localhost:5000/api/users/register';
-  // private apiUrl='https://simpleappbackend-njsq.onrender.com/api/users/register'
+  // private apiUrl = 'http://localhost:5000/api/users/register';
+  // private apiurl = 'https://simpleappbackend-1.onrender.com/api/users/login';
+  // private baseUrl = 'http://localhost:5001/api/posts';
+  // private profileUrl = 'http://localhost:5002/api/profile';
+  // private crushUrl = 'http://localhost:5006/predict';
+  // private profilesection = 'http://localhost:5000/api/users';
+  private apiUrl = 'https://simpleappbackend-1.onrender.com/api/users/register';
   private apiurl = 'https://simpleappbackend-1.onrender.com/api/users/login';
-  private baseUrl = 'http://localhost:5001/api/posts';
-  private profileUrl = 'http://localhost:5002/api/profile';
-  private crushUrl = 'http://localhost:5006/predict';
-  private profilesection = 'http://localhost:5000/api/users';
-  // private apiurl='https://simpleappbackend-njsq.onrender.com/api/users/login'
+  private baseUrl = 'https://simpleappbackend-1.onrender.com/api/posts';
+  private profileUrl = 'https://simpleappbackend-1.onrender.com/api/profile';
+  private crushUrl = 'https://simpleappbackend-1.onrender.com/predict';
+  
   // Your Profile API URL
   constructor(private http: HttpClient,private router: Router) {}
   
@@ -80,6 +84,21 @@ export class AuthserviceService {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this.http.post(this.crushUrl, interactionData, { headers });
   }
+  // addComment(postId: string, commentData: any): Observable<any> {
+  //   return this.http.post<any>(`http://localhost:5001/api/posts/${postId}/comments`, commentData);
+  // }
+  // getUserProfile(userId: string): Observable<any> {
+  //   return this.http.get(`http://localhost:5000/api/users/${userId}`);
+  // }
+  // updateProfilePic(formData: FormData, userId: string): Observable<any> {
+  //   return this.http.put(`http://localhost:5000/api/users/${userId}`, formData);
+  // }
+  // deleteProfilePicture(userId: string): Observable<any> {
+  //   return this.http.delete(`http://localhost:5000/api/users/${userId}/profile-picture`);
+  // }
+  // getUserPosts(userId: string): Observable<any> {
+  //   return this.http.get(`http://localhost:5001/api/posts/user/${userId}`);
+  // }
   addComment(postId: string, commentData: any): Observable<any> {
     return this.http.post<any>(`http://localhost:5001/api/posts/${postId}/comments`, commentData);
   }
